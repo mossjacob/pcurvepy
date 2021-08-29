36573dc3db730f1586da99e97a1c9f8a21bc9a8f
Jacob Moss <cob.mossy@gmail.com>
2021-08-29 14:06:44 +0100

    add func and doc

--- pcurve.py
+++ pcurve.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from scipy.interpolate import UnivariateSpline
 
+
 class PrincipalCurve:
     def __init__(self, k = 3):
         self.k = k
@@ -10,8 +11,14 @@ class PrincipalCurve:
         self.s = None
         self.p_interp = None
         self.s_interp = None
-        
-    def project(self, X, p, s):
+
+    def project(self, X):
+        s_interp, p_interp, d_sq = self.project_on(X, self.p, self.s)
+        self.s_interp = s_interp
+        self.p_interp = p_interp
+        return s_interp, p_interp, d_sq
+
+    def project_on(self, X, p, s):
         '''
         Get interpolating s values for projection of X onto the curve defined by (p, s)
         @param X: data
@@ -21,8 +28,8 @@ class PrincipalCurve:
         '''
         s_interp = np.zeros(X.shape[0])
         p_interp = np.zeros(X.shape)
-        d_sq = 0
-        
+        d_sq = list()
+
         for i in range(0, X.shape[0]):
             z = X[i, :]
             seg_proj = (((p[1:] - p[0:-1]).T)*np.einsum('ij,ij->i', z - p[0:-1], p[1:] - p[0:-1])/np.power(np.linalg.norm(p[1:] - p[0:-1], axis = 1), 2)).T # compute parallel component
@@ -34,8 +41,9 @@ class PrincipalCurve:
             q = seg_proj[idx_min] 
             s_interp[i] = (np.linalg.norm(q)/np.linalg.norm(p[idx_min + 1, :] - p[idx_min, :]))*(s[idx_min+1]-s[idx_min]) + s[idx_min]
             p_interp[i] = (s_interp[i] - s[idx_min])*(p[idx_min+1, :] - p[idx_min, :]) + p[idx_min, :]
-            d_sq = d_sq + np.linalg.norm(proj_dist[idx_min])**2
-            
+            d_sq.append(np.linalg.norm(proj_dist[idx_min])**2)
+
+        d_sq = np.array(d_sq)
         return s_interp, p_interp, d_sq
      
     def renorm_parameterisation(self, p):
@@ -54,16 +62,16 @@ class PrincipalCurve:
         '''
         Fit principal curve to data
         @param X: data
-        @param p: starting curve (optional)
+        @param p: starting curve (optional) if None, then first principal components is used
         @param w: data weights (optional)
         @param max_iter: maximum number of iterations 
         @param tol: tolerance for stopping condition
         @returns: None
         '''
-        pca = sklearn.decomposition.PCA(n_components = X.shape[1])
-        pca.fit(X)
-        pc1 = pca.components_[:, 0]
         if p is None:
+            pca = sklearn.decomposition.PCA(n_components=X.shape[1])
+            pca.fit(X)
+            pc1 = pca.components_[:, 0]
             p = np.kron(np.dot(X, pc1)/np.dot(pc1, pc1), pc1).reshape(X.shape) # starting point for iteration
             order = np.argsort([np.linalg.norm(p[0, :] - p[i, :]) for i in range(0, p.shape[0])])
             p = p[order]
@@ -74,28 +82,28 @@ class PrincipalCurve:
         d_sq_old = np.Inf
         
         for i in range(0, max_iter):
-            s_interp, p_interp, d_sq = self.project(X, p, s)
-            
+            # 1. Project data onto curve and set the pseudotime s_interp to be the arc length of the projections
+            s_interp, p_interp, d_sq = self.project_on(X, p, s)
+            d_sq = d_sq.sum()
             if np.abs(d_sq - d_sq_old) < tol:
                 break
             d_sq_old = d_sq
-            
+
+            # 2. Use pseudotimes (s_interp) to order the data and apply a spline interpolation in each data dimension j
             order = np.argsort(s_interp)
-            # s_interp = s_interp[order]
-            # X = X[order, :]
 
             spline = [UnivariateSpline(s_interp[order], X[order, j], k = self.k, w = w) for j in range(0, X.shape[1])]
 
+            # p is the set of J functions producing a smooth curve in R^J
             p = np.zeros((len(s_interp), X.shape[1]))
             for j in range(0, X.shape[1]):
                 p[:, j] = spline[j](s_interp[order])
 
             idx = [i for i in range(0, p.shape[0]-1) if (p[i] != p[i+1]).any()]
             p = p[idx, :]
-            s = self.renorm_parameterisation(p)
+            s = self.renorm_parameterisation(p)  # normalise to unit speed
             
         self.s = s
         self.p = p
         self.p_interp = p_interp
         self.s_interp = s_interp
-        return

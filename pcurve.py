9c34b4425c88757738118fe0499ca732d07293af
Jacob Moss <cob.mossy@gmail.com>
2021-11-11 11:56:54 +0000

    remove deprecated code

--- pcurve.py
+++ pcurve.py
@@ -24,19 +24,19 @@ class PrincipalCurve:
         self.pseudotimes_interp = None
 
     @staticmethod
-    def from_params(s, p, order=None):
+    def from_params(pseudotime, points, order=None):
         """
         Constructs a PrincipalCurve. If no order given, an ordered input is assumed.
         """
         curve = PrincipalCurve()
-        curve.update(s, p, order=order)
+        curve.update(pseudotime, points, order=order)
         return curve
 
-    def update(self, s_interp, p_interp, order=None):
-        self.pseudotimes_interp = s_interp
-        self.points_interp = p_interp
+    def update(self, pseudotime_interp, points_interp, order=None):
+        self.pseudotimes_interp = pseudotime_interp
+        self.points_interp = points_interp
         if order is None:
-            self.order = np.arange(s_interp.shape[0])
+            self.order = np.arange(pseudotime_interp.shape[0])
         else:
             self.order = order
 
@@ -160,42 +160,6 @@ class PrincipalCurve:
         self.order = new_ord
         return dist_ind, dist
 
-    def _project_on(self, X, points, pseudotimes):
-        """
-        Deprecated
-        Get interpolating s values for projection of X onto the curve defined by (p, s)
-        @param X: data
-        @param points: curve points
-        @param pseudotimes: curve parameterisation
-        @returns: interpolating parameter values, projected points on curve, sum of square distances
-        """
-        s_interp = np.zeros(X.shape[0])
-        p_interp = np.zeros(X.shape)
-        d_sq = list()
-        diff = points[1:] - points[:-1]  # first difference
-        # denominator = np.power(np.linalg.norm(diff, axis=1), 2)
-        length = np.square(diff).sum(axis=1)
-        for i in range(X.shape[0]): # for each point
-            z = X[i, :]  # z is the vector of the dimensions for the point
-            numerator = diff.T * np.einsum('ij,ij->i', z - points[:-1], diff)
-            seg_proj = (numerator / length).T  # compute parallel component
-            proj_dist = z - points[:-1] - seg_proj  # compute perpendicular component
-            dist_endpts = np.minimum(np.linalg.norm(z - points[:-1], axis=1), np.linalg.norm(z - points[1:], axis=1))
-            dist_seg = np.maximum(np.linalg.norm(proj_dist, axis=1), dist_endpts)
-
-            idx_min = np.argmin(dist_seg)
-            q = seg_proj[idx_min]
-            t_diff = pseudotimes[idx_min + 1] - pseudotimes[idx_min]
-            x_diff = points[idx_min + 1, :] - points[idx_min, :]
-            s_interp[i] = (np.linalg.norm(q) / np.linalg.norm(x_diff)) * t_diff + pseudotimes[idx_min]
-            p_interp[i] = (s_interp[i] - pseudotimes[idx_min]) * x_diff + points[idx_min, :]
-
-            d_sq.append(np.linalg.norm(proj_dist[idx_min])**2)
-
-        d_sq = np.array(d_sq)
-        self.order = s_interp.argsort()
-        return s_interp, p_interp, d_sq
-
     def unpack_params(self):
         return self.pseudotimes_interp, self.points_interp, self.order
 
@@ -259,51 +223,5 @@ class PrincipalCurve:
                 break
             d_sq_old = d_sq
 
-        self.pseudotimes = s
-        self.points = p
-
-    def old_fit(self, X, p=None, w=None, max_iter=10, tol=1e-3):
-        """
-        Fit principal curve to data
-        @param X: data
-        @param p: starting curve (optional) if None, then first principal components is used
-        @param w: data weights (optional)
-        @param max_iter: maximum number of iterations
-        @param tol: tolerance for stopping condition
-        @returns: None
-        """
-        if p is None:
-            pca = sklearn.decomposition.PCA(n_components=X.shape[1])
-            pca.fit(X)
-            pc1 = pca.components_[:, 0]
-            p = np.kron(np.dot(X, pc1)/np.dot(pc1, pc1), pc1).reshape(X.shape) # starting point for iteration
-            order = np.argsort([np.linalg.norm(p[0, :] - p[i, :]) for i in range(0, p.shape[0])])
-            p = p[order]
-        s = self.renorm_parameterisation(p)
-        
-        d_sq_old = np.Inf
-        
-        for i in range(0, max_iter):
-            # 1. Project data onto curve and set the pseudotime s_interp to be the arc length of the projections
-            _, dist_ind, d_sq = self.project_to_curve(X, points=p, pseudotimes=s) # s not used?
-            s_interp, p_interp = self.pseudotimes_interp, self.points_interp
-            d_sq = d_sq.sum()
-            if np.abs(d_sq - d_sq_old) < tol:
-                break
-            d_sq_old = d_sq
-
-            # 2. Use pseudotimes (s_interp) to order the data and apply a spline interpolation in each data dimension j
-            order = self.order
-            spline = [UnivariateSpline(s_interp[order], X[order, j], k=self.k, w=w) for j in range(0, X.shape[1])]
-
-            # p is the set of J functions producing a smooth curve in R^J
-            p = np.zeros((len(s_interp), X.shape[1]))
-            for j in range(0, X.shape[1]):
-                p[:, j] = spline[j](s_interp[order])
-
-            idx = [i for i in range(0, p.shape[0]-1) if (p[i] != p[i+1]).any()] # remove duplicate consecutive points?
-            p = p[idx, :]
-            s = self.renorm_parameterisation(p)  # normalise to unit speed
-            
         self.pseudotimes = s
         self.points = p
